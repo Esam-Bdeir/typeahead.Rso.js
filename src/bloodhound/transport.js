449390f3dfd8f60df3b38f3f931938edf2f18466
Esam Bdeir <esam.bdeir@lexmark.com>
2016-11-15 15:25:39 +0100

    fix failed unit tests (transport_spec)

--- src/bloodhound/transport.js
+++ src/bloodhound/transport.js
@@ -49,7 +49,8 @@ var Transport = (function() {
       o = o || {};
       //$.param(o.data || {}) crashes because it does not expect a single string value.
       //if o.data is string no need to call param
-      return o.url + o.type + _.isString(o.data) ? o.data : $.param(o.data || {});
+
+	  return o.url + o.type + (o.data &&_.isString(o.data) ? o.data : $.param(o.data || {}));
     },
 
     _get: function(o, cb) {
